fb12049cd15d4483231715c755db9e3e1097656b
Chandrashekhar Chavan <chandrashekharcorp@gmail.com>
2025-07-09 20:35:07 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ export default function Home(){
     <>
     <div className="main">hello</div>
     <div className="main">how are you</div>
+
+    <div className="box  w-96 h-96 bg-slate-600"></div>
+
+
   </>
   )
 }
