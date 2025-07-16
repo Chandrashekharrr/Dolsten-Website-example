d4f4d281293cce398b624dd310b24c21f2e426d9
Chandrashekhar <chandrashekharcorp@gmail.com>
2025-07-16 21:18:10 +0530

    again checked

--- src/app/components/Model.tsx
+++ src/app/components/Model.tsx
@@ -1,5 +1,7 @@
 import { IBM_Plex_Mono } from "next/font/google";
 import localFont from "next/font/local";
+import Image from "next/image";
+import React from "react";
 
 const plexMono = IBM_Plex_Mono({
     weight: "700",
@@ -33,7 +35,7 @@ export default function Model() {
                         <div className="colOne  w-full px-5">
                             <div className="whiteSpace w-full bg-white p-4 flex flex-col sm:flex-row lg:flex-col justify-between items-center sm:gap-10">
 
-                                <div className="imageDiv overflow-hidden"><img src="./Images/model1.webp" alt="" className="object-cover " /></div>
+                                <div className="imageDiv overflow-hidden"><Image src="./Images/model1.webp" alt="" className="object-cover " /></div>
 
                                 <div className="TextContentDiv overflow-hidden flex flex-col justify-evenly">
 
@@ -60,7 +62,7 @@ export default function Model() {
                         <div className="colOne  w-full px-5">
                             <div className="whiteSpace w-full bg-white p-4 flex flex-col sm:flex-row lg:flex-col justify-between items-center sm:gap-10">
 
-                                <div className="imageDiv overflow-hidden"><img src="./Images/model2.webp" alt="" className="object-cover " /></div>
+                                <div className="imageDiv overflow-hidden"><Image src="./Images/model2.webp" alt="" className="object-cover " /></div>
 
                                 <div className="TextContentDiv overflow-hidden flex flex-col justify-evenly">
 
@@ -89,7 +91,7 @@ export default function Model() {
                         <div className="colOne  w-full px-5">
                             <div className="whiteSpace w-full bg-white p-4 flex flex-col sm:flex-row lg:flex-col justify-between items-center sm:gap-10">
 
-                                <div className="imageDiv overflow-hidden"><img src="./Images/model3.webp" alt="" className="object-cover " /></div>
+                                <div className="imageDiv overflow-hidden"><Image src="./Images/model3.webp" alt="" className="object-cover " /></div>
 
                                 <div className="TextContentDiv overflow-hidden flex flex-col justify-evenly">
 
@@ -118,7 +120,7 @@ export default function Model() {
                         <div className="colOne  w-full px-5">
                             <div className="whiteSpace w-full bg-white p-4 flex flex-col sm:flex-row lg:flex-col justify-between items-center sm:gap-10">
 
-                                <div className="imageDiv overflow-hidden"><img src="./Images/model4.webp" alt="" className="object-cover " /></div>
+                                <div className="imageDiv overflow-hidden"><Image src="./Images/model4.webp" alt="" className="object-cover " /></div>
 
                                 <div className="TextContentDiv overflow-hidden flex flex-col justify-evenly">
 
